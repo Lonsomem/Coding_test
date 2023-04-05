f1c70177627185989e856ecfc87c8f9aa9df2361
Lonsomem <hclee0730@gmail.com>
2023-04-05 13:05:22 +0900

    오류나긴 하는데 일단 커밋 ㅋㅋ

--- 1차시/2번문제.py
+++ 1차시/2번문제.py
@@ -1,24 +1,8 @@
-global answer
-answer = [0,0]
-def solution_tree(arr, x, y, size):
-    
-    if size == 1:
-        answer[criteria] += 1
-    else:
-        for px in range(x, x+size):
-            for py in range(y, y+size):
-                if arr[px][py] != criteria:
-                    solution_tree(arr, x, y, size//2)
-                    solution_tree(arr, x+size//2, y, size//2)
-                    solution_tree(arr, x, y+size//2, size//2)
-                    solution_tree(arr, x+size//2, y+size//2, size//2)
-                else:
-                    if px == x+size-1 and py == y+size-1:
-                        solution_tree(arr, x, y, 1)
-                
 def solution(arr):
-    solution_tree(arr, 0, 0, len(arr))
-    return answer
+    def quad_tree(arr, x, y, size):
+        
 
-ans = solution([[1,1,0,0],[1,0,0,0],[1,0,0,1],[1,1,1,1]])
-print(ans)
+    
+    
+    answer = []
+    return answer
--- 2차시/1번문제.py
+++ 2차시/1번문제.py
@@ -1,34 +1,27 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/64061
 
 def solution(board, move):
-    basket = []                                  # 바구니
-
-    for c in move:                               # 움직임 순번; 인덱스+1;
-        if board[c-1][-1] == 0:
-            while 0 not in board[c-1]:       # 0인 인형 없을 때까지
-        
-        
-        if basket != []:                     # 바구니가 비어있지 않을 때
-            if board[c-1][-1] == basket[-1]: # 바구니랑 뽑은 인형 같을 떄
-                basket.pop()
-            else:                            # 바구니랑 뽑은 인형 다를 때
-                basket.append(board[c-1][-1])
-                board[c-1].pop()
-        else:                                # 바구니 비어있을 때
-            basket.apend(board[c-1][-1])
-
-
-                
-
-        
-        
-        
-
-
-
-
-
-
-
     answer = 0
+    basket = []
+    board1 = [[] for _ in range(len(board))]
+    for col in range(len(board)):               # board 역으로 정렬
+        for element_id in range(len(board)):
+            if board[col][element_id] != 0:
+                board1[element_id].insert(0, board[col][element_id])
+            else:
+                continue                         
+    for c in move:                               # 움직임 순번; 인덱스+1;
+        if board1[c-1] != []:                     # board 비어있지 않을 때
+            if basket != []:                     # 바구니가 비어있지 않을 때
+                if board1[c-1][-1] == basket[-1]: # 바구니랑 뽑은 인형 같을 떄
+                    basket.pop()
+                    answer += 2
+                else:                            # 바구니랑 뽑은 인형 다를 때
+                    basket.append(board1[c-1][-1])
+                    board1[c-1].pop()
+            else:                                # 바구니 비어있을 때
+                basket.append(board1[c-1][-1])
+                board1[c-1].pop()
+        else:                                    # board 비어있을 때
+            continue
     return answer
